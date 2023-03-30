a3bd50d22aeaa75d3bf452925374babf63c6f668
Dar-Ci Calhoun <dar-ci.calhoun@meraki.net>
2023-03-30 11:30:56 -0700

    table names should be plural

--- db/Kitten.js
+++ db/Kitten.js
@@ -1,6 +1,6 @@
 const {Sequelize, sequelize} = require('./db');
 
-const Kitten = sequelize.define('kitten', {
+const Kitten = sequelize.define('kittens', {
   name: Sequelize.STRING,
   color: Sequelize.STRING,
   age: Sequelize.INTEGER
