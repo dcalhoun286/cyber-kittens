81cd67154e3db85e96742a93d02cb004cf69de57
Dar-Ci Calhoun <dar-ci.calhoun@meraki.net>
2023-03-30 11:34:57 -0700

    installed dotenv and moved PORT variable into env file

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const app = require('./index');
 const { sequelize } = require('./db');
 
-const { PORT = 4000 } = process.env;
+require('dotenv').config();
+
+const PORT = process.env.PORT | 4000;
 
 app.listen(PORT, () => {
   sequelize.sync({ force: false });
